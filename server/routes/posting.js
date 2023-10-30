18faaeea0b37235aa96de435160755897e0d9667
Septi Rizki <septirizky79@gmail.com>
2023-10-30 16:51:37 +0700

    tambah route search

--- server/routes/posting.js
+++ server/routes/posting.js
@@ -6,5 +6,6 @@ postingRoute.post("/createPost", PostingController.createPosting);
 postingRoute.get("/detail/:id", PostingController.detailPosting);
 postingRoute.put("/update/:id", PostingController.update);
 postingRoute.delete("/delete/:id", PostingController.delete);
+postingRoute.get("/search/:keyword", PostingController.searching);
 
 module.exports = postingRoute;
