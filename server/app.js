47b1f9ccdbde3f43055cef6448d8c6edee1c5e1c
Septi Rizki <septirizky79@gmail.com>
2023-10-30 15:41:12 +0700

    perubahan di app.js

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const route = require("./routes");
 const AuthMiddlewares = require("./middlewares/AuthMiddlewares");
+
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 7200;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 app.use(AuthMiddlewares);
 app.use(route);
 app.listen(port, () => {
